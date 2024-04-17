dfb8efa6244d34f40ab89b269c2ddf458becf058
Rakim Middya <103818757+rakim-0@users.noreply.github.com>
2024-04-17 20:15:15 +0530

    First Upload

---
+++ frontend/src/component/Footer.js
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import React from "react";
+import { useTheme } from "@mui/material/styles";
+
+const Footer = () => {
+    const { palette } = useTheme();
+    return <></>;
+};
+
+export default Footer;
